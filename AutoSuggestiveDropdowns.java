52771f167e399acbadb310a48d37286954bb7c76
vijay111shetty <67384977+vijay111shetty@users.noreply.github.com>
2022-01-30 17:31:29 +0530

    Update AutoSuggestiveDropdowns.java

--- AutoSuggestiveDropdowns.java
+++ AutoSuggestiveDropdowns.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class AutoSuggestiveDropdowns {
 
 	public static void main(String[] args) throws InterruptedException {
